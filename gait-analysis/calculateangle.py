e2f740a337ae83cd1b58421e1b4934a5da3f7b45
daryllavin <daryllavin@gmail.com>
2025-07-23 11:56:05 -0400

    removed individual landmark prompts

--- gait-analysis/calculateangle.py
+++ gait-analysis/calculateangle.py
@@ -45,7 +45,7 @@ def plot_point_over_time(data, label, save_path):
     data = np.array(data)
     frames = np.arange(data.shape[0]) / 60
     plt.figure(figsize=(10, 6))
-    plt.plot(frames, data[:, 2], label='Z')
+    plt.plot(frames, data[:, 2])
     plt.title(f'Z Distance Over Time ({label})')
     plt.xlabel('Time (s)')
     plt.ylabel('Z Distance (m)')
@@ -58,7 +58,7 @@ def plot_point_over_time(data, label, save_path):
 def plot_angle_over_time(angles, bp, save_path):
     frames = np.arange(len(angles)) / 60
     plt.figure(figsize=(10, 6))
-    plt.plot(frames, angles, label='Angle', color='purple')
+    plt.plot(frames, angles, color='purple')
     plt.title(f'{bp.capitalize()} Angle Over Time')
     plt.xlabel('Time (s)')
     plt.ylabel('Angle (degrees)')
@@ -72,56 +72,57 @@ def main(folder=None, bp=None):
     # If not being used as module get the input folder and requested body part
     if folder is None:
         folder = input("Folder name: ")
-    if bp is None:
-        bp = input("Body part (left knee, right knee, left elbow, right elbow, left hip, right hip): ")
+    
+    for bp in landmark_map:
+        # Get the three landmarks based on the landmark at which angle is being calculated
+        bp1, bp2, bp3 = landmark_map[bp]
 
-    # Get the three landmarks based on the landmark at which angle is being calculated
-    bp1, bp2, bp3 = landmark_map[bp]
+        CACHE_FILE = f'{folder}/{bp}/landmark_cache.npz'
 
-    CACHE_FILE = f'{folder}/{bp}/landmark_cache.npz'
+        # If pipelandmark has already been run, use the cache data
+        if os.path.exists(CACHE_FILE):
+            data = np.load(CACHE_FILE)
+            l1 = data['l1']
+            l2 = data['l2']
+            l3 = data['l3']
+            input1 = data['input1'].tolist()
+            input2 = data['input2'].tolist()
+            input3 = data['input3'].tolist()
+            print("Loaded from cache")
+        # If pipelandmark hasn't been run, run it
+        else:
+            print("Running pipelandmark...")
+            from pipelandmark import extract_landmarks
+            l1, l2, l3, input1, input2, input3 = extract_landmarks(folder, bp1, bp2, bp3)
 
-    # If pipelandmark has already been run, use the cache data
-    if os.path.exists(CACHE_FILE):
-        data = np.load(CACHE_FILE)
-        l1 = data['l1']
-        l2 = data['l2']
-        l3 = data['l3']
-        input1 = data['input1'].tolist()
-        input2 = data['input2'].tolist()
-        input3 = data['input3'].tolist()
-        print("Loaded from cache")
-    # If pipelandmark hasn't been run, run it
-    else:
-        print("Running pipelandmark...")
-        from pipelandmark import extract_landmarks
-        l1, l2, l3, input1, input2, input3 = extract_landmarks(folder, bp1, bp2, bp3)
+            # Save cache file for this data so pipelandmark doesn't have to be run on it again
+            np.savez(CACHE_FILE, l1=l1, l2=l2, l3=l3, input1=input1, input2=input2, input3=input3)
 
-        # Save cache file for this data so pipelandmark doesn't have to be run on it again
-        np.savez(CACHE_FILE, l1=l1, l2=l2, l3=l3, input1=input1, input2=input2, input3=input3)
+        l4 = []
+        for i in range(len(l1)):
+            l4.append(calculate_angle(l2[i], l1[i], l3[i]))
 
-    l4 = []
-    for i in range(len(l1)):
-        l4.append(calculate_angle(l2[i], l1[i], l3[i]))
+        # Save the x/y/z for each body part and the angle data as csv files
+        save_dir = f'charts/{folder}/data'
+        os.makedirs(save_dir, exist_ok=True)
+        np.savetxt(f'charts/{folder}/data/{input1}.csv', l1, delimiter=',', fmt='%s')
+        np.savetxt(f'charts/{folder}/data/{input2}.csv', l2, delimiter=',', fmt='%s')
+        np.savetxt(f'charts/{folder}/data/{input3}.csv', l3, delimiter=',', fmt='%s')
+        np.savetxt(f'charts/{folder}/data/{input1} angle.csv', l4, delimiter=',', fmt='%s')
 
-    # Save the x/y/z for each body part and the angle data as csv files
-    save_dir = f'charts/{folder}'
-    os.makedirs(save_dir, exist_ok=True)
-    np.savetxt(f'charts/{folder}/{input1}.csv', l1, delimiter=',', fmt='%s')
-    np.savetxt(f'charts/{folder}/{input2}.csv', l2, delimiter=',', fmt='%s')
-    np.savetxt(f'charts/{folder}/{input3}.csv', l3, delimiter=',', fmt='%s')
-    np.savetxt(f'charts/{folder}/{input3} angle.csv', l4, delimiter=',', fmt='%s')
+        # Plot and save charts for each point's x, y, z over time
+        os.makedirs(f"charts/{folder}/graphs", exist_ok=True)
+        plot_point_over_time(l1, input1, f'charts/{folder}/graphs/{input1} distance.png')
+        plot_point_over_time(l2, input2, f'charts/{folder}/graphs/{input2} distance.png')
+        plot_point_over_time(l3, input3, f'charts/{folder}/graphs/{input3} distance.png')
 
-    return folder, input1, input2, input3, l1, l2, l3, l4
+        # Plot and save angle over time
+        plot_angle_over_time(l4, input1, f'charts/{folder}/graphs/{input1} angle.png')
+
+    return l4
 
 if __name__ == '__main__':
-    folder, input1, input2, input3, l1, l2, l3, l4 = main()
-    # Plot and save charts for each point's x, y, z over time
-    plot_point_over_time(l1, input1, f'charts/{folder}/{input1} distance.png')
-    plot_point_over_time(l2, input2, f'charts/{folder}/{input2} distance.png')
-    plot_point_over_time(l3, input3, f'charts/{folder}/{input3} distance.png')
-
-    # Plot and save angle over time
-    plot_angle_over_time(l4, input1, f'charts/{folder}/{input1} angle.png')
+    l4 = main()
 
     # If not being run as a module, print the angle at each frame
     for angle in l4:
--- gait-analysis/detrend.py
+++ gait-analysis/detrend.py
@@ -6,41 +6,40 @@ import calculateangle
 
 # Get folder
 folder = input("Folder name: ")
-# Get body part
-bp = input("Body part: ")
+for bp in ('left elbow', 'left shoulder', 'left wrist', 'left knee', 'left hip', 'left ankle', 'right elbow', 'right shoulder', 'right wrist', 'right knee', 'right hip', 'right ankle'):
+    if not os.path.exists(f"charts/{folder}/data/{bp}.csv"):
+        calculateangle.main(folder, bp)
 
-if not os.path.exists(f"charts/{folder}/{bp}.csv"):
-    calculateangle.main(folder, bp)
+    # Read z data from csv file for that body part
+    df = pd.read_csv(f"charts/{folder}/data/{bp}.csv", usecols=[2])
+    df.columns = [bp]
 
-# Read z data from csv file for that body part
-df = pd.read_csv(f"charts/{folder}/{bp}.csv", usecols=[2])
-df.columns = [bp]
+    # Align time data (assuming video is 60 fps)
+    df['Time'] = df.index / 60
+    df = df[['Time', bp]]
 
-# Align time data (assuming video is 60 fps)
-df['Time'] = df.index / 60
-df = df[['Time', bp]]
+    # Get arrays of time and distance data
+    x = df['Time'].values
+    y = df[bp].values
 
-# Get arrays of time and distance data
-x = df['Time'].values
-y = df[bp].values
+    # Create trendline
+    degree = 2 
+    coeffs = np.polyfit(x, y, degree)
+    trend = np.polyval(coeffs, x)
 
-# Create trendline
-degree = 2 
-coeffs = np.polyfit(x, y, degree)
-trend = np.polyval(coeffs, x)
+    # Get detrended data by finding difference between actual and trend data
+    df['Detrended'] = y - trend
 
-# Get detrended data by finding difference between actual and trend data
-df['Detrended'] = y - trend
-
-# Create plot with distance, trend, and detrended data over time
-plt.figure(figsize=(10, 6))
-plt.plot(x, y, label='Original')
-plt.plot(x, trend, label='Trend', linestyle='--')
-plt.plot(x, df['Detrended'], label='Detrended', linestyle=':')
-plt.legend()
-plt.xlabel("Time (s)")
-plt.ylabel(f"{bp.capitalize()} Value")
-plt.title(f"Polynomial Detrending - {bp.capitalize()}")
-plt.grid(True)
-plt.tight_layout()
-plt.savefig(f"{folder.capitalize()} {bp.capitalize()} Detrended.png")
+    # Create plot with distance, trend, and detrended data over time
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, y, label='Original')
+    plt.plot(x, trend, label='Trend', linestyle='--')
+    plt.plot(x, df['Detrended'], label='Detrended', linestyle=':')
+    plt.legend()
+    plt.xlabel("Time (s)")
+    plt.ylabel(f"{bp.capitalize()} Value")
+    plt.title(f"Polynomial Detrending - {bp.capitalize()}")
+    plt.grid(True)
+    plt.tight_layout()
+    os.makedirs(f"charts/{folder}/detrended", exist_ok=True)
+    plt.savefig(f"charts/{folder}/detrended/{bp} Detrended.png")
